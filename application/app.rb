40b12b9500e4dea70802f288b80f12e95d2c7694
Wenhao Ni <niwenhao@gmail.com>
2016-06-22 22:31:11 +0900

    up

--- application/app.rb
+++ application/app.rb
@@ -1,5 +1,12 @@
 require "sinatra"
 
-get /video/* do |path|
-    "Hello #{path}"
+VIDEO_DIR=/video/
+
+get '/video/*' do |path|
+    path = VIDEO_DIR + path
+    if File.directory? path
+        render_directory path
+    else
+        render_file path
+    end
 end
